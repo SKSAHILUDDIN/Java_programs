511028bb2610c8b044bfbbcc66eadb757b222c16
SK SAHILUDDIN <sksahiluddin68@gmail.com>
2026-08-29 12:04:45 +0530

    Newly added comparing methods

--- src/com/company/Rock_paper_Scissors_game.java
+++ src/com/company/Rock_paper_Scissors_game.java
@@ -1,20 +1,11 @@
 package com.company;
-import org.w3c.dom.ls.LSInput;
-
-import javax.swing.*;
-import java.lang.classfile.PseudoInstruction;
 import java.util.Scanner;
 import java.util.Random;
 public class Rock_paper_Scissors_game {
-    static void Computer() {
+    static int Computer() {
         Random random = new Random();
         int randomIntBounded = random.nextInt(0,3);
-        if (randomIntBounded== 0){
-            System.out.println("Rock");}
-        else if  (randomIntBounded== 1){
-            System.out.println("Paper");}
-        else if (randomIntBounded== 2){
-            System.out.println("Scissor");}
+        return randomIntBounded;
 
     }
     public static void main(String[] args){
@@ -26,18 +17,22 @@ public class Rock_paper_Scissors_game {
         Computer();
 
         Random random = new Random();
-        int randomIntBounded = random.nextInt(0,3);
+       /* int randomIntBounded = random.nextInt(0,3);
         if (randomIntBounded== 0){
             System.out.println("Rock");}
         else if  (randomIntBounded== 1){
             System.out.println("Paper");}
         else if (randomIntBounded== 2){
-            System.out.println("Scissor");}
+            System.out.println("Scissor");}*/
+        System.out.println("rock= 0 , Paper = 1, Scissor = 2");
         for (int i=0;i<5;i++){
-            String r = "rock" , p = "Paper", s = "Scissor" ;
-            System.out.println("r = rock , p = Paper, s = Scissor \n Enter your choice: ");
-            String ch = sc.nextLine();
-            if(ch==r && randomIntBounded==p)
+            System.out.printf(" Enter your choice: ");
+            int ch = sc.nextInt();
+            if(ch==0 && Computer()==0) System.out.println("Draw");
+            if(ch==0 && Computer()==1) System.out.println("Computer wins");
+            if(ch==0 && Computer()==2) System.out.println("You win");
+            if(ch==1 && Computer()==0) System.out.println("You win");
+            if(ch==2 && Computer()==0) System.out.println("Computer wins");
         }
 
 
