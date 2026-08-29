1e4f03a9c03e48cb4a1b3c055e182419aa6982a7
SK SAHILUDDIN <sksahiluddin68@gmail.com>
2026-08-29 14:33:40 +0530

    Newly added comparing methods

--- src/com/company/Rock_paper_Scissors_game.java
+++ src/com/company/Rock_paper_Scissors_game.java
@@ -14,9 +14,6 @@ public class Rock_paper_Scissors_game {
         System.out.println("**---------- We have 5 rounds of the game----------**  \n ----------Lets start----------");
 
         Scanner sc = new Scanner(System.in);
-        Computer();
-
-        Random random = new Random();
        /* int randomIntBounded = random.nextInt(0,3);
         if (randomIntBounded== 0){
             System.out.println("Rock");}
@@ -25,15 +22,28 @@ public class Rock_paper_Scissors_game {
         else if (randomIntBounded== 2){
             System.out.println("Scissor");}*/
         System.out.println("rock= 0 , Paper = 1, Scissor = 2");
+        int winningrateforcomputer=0;
+        int winningrateforyou=0;
         for (int i=0;i<5;i++){
-            System.out.printf(" Enter your choice: ");
+            System.out.println(" Enter your choice: ");
             int ch = sc.nextInt();
-            if(ch==0 && Computer()==0) System.out.println("Draw");
-            if(ch==0 && Computer()==1) System.out.println("Computer wins");
-            if(ch==0 && Computer()==2) System.out.println("You win");
-            if(ch==1 && Computer()==0) System.out.println("You win");
-            if(ch==2 && Computer()==0) System.out.println("Computer wins");
+            System.out.println("Computer choice: "+ Computer());
+
+            if((ch==0 && Computer()==0) ||(ch==1 && Computer()==1)||(ch==2 && Computer()==2)){
+                System.out.println("Draw");
+            }
+            if((ch==0 && Computer()==1)||(ch==2 && Computer()==0)) {
+                System.out.println("Computer wins");
+                winningrateforcomputer++;
+            }
+            if((ch==0 && Computer()==2)||(ch==1 && Computer()==0)) {
+                System.out.println("You win");
+                winningrateforyou++;
+            }
+
         }
+        if(winningrateforcomputer>winningrateforyou) System.out.println("Computer won the game");
+        else if(winningrateforcomputer<winningrateforyou) System.out.println("You won the game");
 
 
 
